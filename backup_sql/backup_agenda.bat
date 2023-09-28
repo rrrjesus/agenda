77149b848e8e7b23902bfd9758826a36e8380255
Rodolfo Romaioli Ribeiro de Jesus <rrrjesus@users.noreply.github.com>
2023-09-28 11:18:21 -0300

    .bat backup SEO Online e Access
    .bat backup SEO Online e Access

--- backup_sql/backup_agenda.bat
+++ backup_sql/backup_agenda.bat
@@ -3,4 +3,6 @@ c:\xampp\mysql\bin\mysqldump --single-transaction=TRUE -u smsubcoti -pyvTjF3VLK)
 c:\xampp\mysql\bin\mysqldump --single-transaction=TRUE -u smsubcoti -pyvTjF3VLK)RktC7W smsub sectors > Z:\sectors\sectors_%date:~0,2%%date:~3,2%%date:~6,4%.sql
 c:\xampp\mysql\bin\mysqldump --single-transaction=TRUE -u smsubcoti -pyvTjF3VLK)RktC7W smsub posts > Z:\posts\posts_%date:~0,2%%date:~3,2%%date:~6,4%.sql
 c:\xampp\mysql\bin\mysqldump --single-transaction=TRUE -u smsubcoti -pyvTjF3VLK)RktC7W smsub contacts > Z:\contacts\contacts_%date:~0,2%%date:~3,2%%date:~6,4%.sql
+c:\xampp\mysql\bin\mysqldump --single-transaction=TRUE -u smsubcoti -pyvTjF3VLK)RktC7W smsub report_access > Z:\report_access\report_access_%date:~0,2%%date:~3,2%%date:~6,4%.sql
+c:\xampp\mysql\bin\mysqldump --single-transaction=TRUE -u smsubcoti -pyvTjF3VLK)RktC7W smsub report_online > Z:\report_online\report_online_%date:~0,2%%date:~3,2%%date:~6,4%.sql
 echo Backup concluído com sucesso.
