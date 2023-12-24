42ff67eb718d2b5d4358485c2dbd330654d537e2
Rodolfo Romaioli Ribeiro de Jesus <rrrjesus@users.noreply.github.com>
2023-12-23 23:48:26 -0300

    update

--- themes/smsubpanel/assets/js/scripts.js
+++ themes/smsubpanel/assets/js/scripts.js
@@ -1,3 +1,184 @@
+$(document).ready(function() {
+
+$.validator.setDefaults({
+    highlight: function (element, errorClass, validClass) {
+        $(element).addClass('is-invalid');
+    },
+    unhighlight: function (element, errorClass, validClass) {
+        $(element).removeClass('is-invalid');
+        $(element).addClass('is-valid');
+    },
+
+    // errorElement: 'span',
+    errorClass: 'help-block',
+
+    errorPlacement: function (error, element) {
+        error.addClass('invalid-feedback');
+        if (element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        }
+        else if (element.prop('type') === 'radio' && element.parent('.radio-inline').length) {
+            error.insertAfter(element.parent().parent());
+        }
+        else if (element.prop('type') === 'checkbox' || element.prop('type') === 'radio') {
+            error.appendTo(element.parent().parent());
+        }
+        else if (element.prop('type') === 'password') {
+            error.appendTo(element.parent());
+        }
+        else if (element.prop('type') === 'file') {
+            error.appendTo(element.parent());
+        }
+        if (element.parent('select').length) {
+            error.insertAfter(element.parent());
+        }
+        else {
+            error.insertAfter(element);
+        }
+    }
+});
+
+$("#contact-register").validate({
+    rules: {
+        sector: {
+            required: true
+            // remote: "remote/valida-email.php"
+        },
+        collaborator: {
+            required: true
+        },
+        ramal: {
+            required: true,
+            maxlength: 4
+        }
+    },
+    messages: {
+        sector: {
+            required: "Digite o setor !!!"
+            // remote: "Email não encontrado !!!"
+        },
+        collaborator: {
+            required: "Digite o nome !!!"
+        },
+        ramal: {
+            required: "Digite o ramal !!!",
+            maxlength: "Ramais possuem apenas 4 dígitos !!!"
+        }
+    }
+});
+
+$("#sector-register").validate({
+    rules: {
+        sector: {
+            required: true
+        }
+    },
+    messages: {
+        sector: {
+            required: "Digite o setor !!!"
+        }
+    }
+});
+
+$("#contact-edit").validate({
+    rules: {
+        sector: {
+            required: true
+            // remote: "remote/valida-email.php"
+        },
+        collaborator: {
+            required: true
+        },
+        ramal: {
+            required: true
+        }
+    },
+    messages: {
+        sector: {
+            required: "Digite o setor !!!"
+            // remote: "Email não encontrado !!!"
+        },
+        collaborator: {
+            required: "Digite o nome !!!"
+        },
+        ramal: {
+            required: "Digite o ramal !!!"
+        }
+    }
+});
+
+$("#user-updated").validate({
+    rules: {
+        first_name: {
+            required: true
+            // remote: "remote/valida-email.php"
+        },
+        last_name: {
+            required: true
+        },
+        email: {
+            required: true
+        },
+        functional_record: {
+            required: true
+        }
+    },
+    messages: {
+        first_name: {
+            required: "Digite o nome !!!"
+            // remote: "Email não encontrado !!!"
+        },
+        last_name: {
+            required: "Digite o sobrenome !!!"
+        },
+        email: {
+            required: "Digite o e-mail !!!"
+        },
+        functional_record: {
+            required: "Digite o RF !!!"
+        }
+    }
+});
+
+$("#user-profile").validate({
+    rules: {
+        first_name: {
+            required: true
+            // remote: "remote/valida-email.php"
+        },
+        last_name: {
+            required: true
+        },
+        email: {
+            required: true
+        },
+        functional_record: {
+            required: true
+        }
+    },
+    messages: {
+        first_name: {
+            required: "Digite o nome !!!"
+            // remote: "Email não encontrado !!!"
+        },
+        last_name: {
+            required: "Digite o sobrenome !!!"
+        },
+        email: {
+            required: "Digite o e-mail !!!"
+        },
+        functional_record: {
+            required: "Digite o RF !!!"
+        }
+    }
+});
+
+//  data-bs-toggle="tooltip" Bootstrap Title
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-togglee="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
 $(function () {
     $("form").submit(function (e) {
         e.preventDefault();
@@ -30,4 +211,5 @@ $(function () {
             }
         });
     });
+});
 });
